397ae6159cd541d1df53ec40b306d1f0eb641151
antoineabln <johndoe@example.com>
2023-02-22 06:32:22 +0100

    programme qui découpe une chaîne de caractères en tableau en fonction du séparateur donné en 2e argument

---
+++ eau02.js
@@ -0,0 +1,11 @@
+let str = "Crevette magique dans la mer des étoiles";
+let separateur = "la";
+
+function split(){
+  
+  return str.split(separateur);
+    
+  }
+
+
+console.log(split(str,separateur));
